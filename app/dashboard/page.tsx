8894aa0bddd10078a884b0bbf39d2676faad10c0
Rachit Dhaka <rachitdhaka29@gmail.com>
2026-01-18 03:28:11 +0530

    changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,13 +11,15 @@ import {
 export default function Page() {
   return (
     <div className="flex  gap-2 h-screen p-2 bg-neutral-200 dark:bg-neutral-900">
-      
-
-
-        
+      <SignedIn>
         <ClusterMap />
         <ContentSideBar />
-      
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal" oauthFlow="popup">login</SignInButton>
+        <SignUpButton mode="modal" oauthFlow="popup">signup</SignUpButton>
+      </SignedOut>
     </div>
   );
 }

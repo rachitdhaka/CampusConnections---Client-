2229a34b407d0bfd37f4a66a3630d6bc93100471
Rachit Dhaka <rachitdhaka29@gmail.com>
2026-01-20 00:20:18 +0530

    Profile page under dev

---
+++ app/Profile/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Page() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-[60vh] text-center">
+        <h1 className="text-3xl font-bold tracking-tight">Under Development</h1>
+        <p className="mt-2 text-muted-foreground">
+          We're working hard to bring you this page. Please check back soon!
+        </p>
+      </div>
+        
+    </div>
+     
+  );
+}
